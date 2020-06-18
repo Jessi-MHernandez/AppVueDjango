aad76902610d5f26f2238f1f67e7dff1c159907a
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-06-18 17:56:36 -0500

    Empleando ciclos for en Vue

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="box" v-for="e in elements" v-bind:key="e">
+            {{e}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            elements: ["Uno", "Dos", "Tres"]
+        };
+    }
+}
+</script>
+<style>
+    .box{
+        border: 2px solid #cccccc;
+        margin: 2px 0 0 0;
+    }
+</style>
