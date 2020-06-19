45a2c3ee54a30dd43aa804351abaa86f43b02bd5
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-06-18 20:25:52 -0500

    Utilizando Vue Router y habilitand ola navegación entre componentes (SPA)

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,25 @@
 
   <!--  <img alt="Vue logo" src="./assets/logo.png"> -->
     <Header/>
-    <List></List>
+
+      <router-link to="/">Lista</router-link>
+      <router-link to="/detail">Detalles</router-link>
+
+     <router-view></router-view>
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
-import List from './components/List.vue'
+//import List from './components/List.vue'
 //import Detail from './components/Detail.vue'
 //import List from './components/Table.vue'
 
 export default {
   name: 'App',
   components: {
-    List,
+  //  List,
     Header
   //  Detail
   }
