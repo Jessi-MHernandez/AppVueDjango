ffeddfb96f46aee63c1401d5f76ca12365a1b5d5
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-06-18 20:06:46 -0500

    Creando una página para el detalle de un componente

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+
   <!--  <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Header/>
     <List></List>
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 import List from './components/List.vue'
 //import Detail from './components/Detail.vue'
 //import List from './components/Table.vue'
@@ -15,18 +18,8 @@ export default {
   name: 'App',
   components: {
     List,
+    Header
   //  Detail
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <b-navbar type="dark" variant="dark" class="mb-4">
+            <b-navbar-brand>Jess M. Hernandez</b-navbar-brand>
+
+            <b-navbar-nav>
+                <b-nav-item href="#">Home</b-nav-item>
+            
+                <b-nav-item-dropdown text="User" right>
+                    <b-dropdown-item href="#">Account</b-dropdown-item>
+                    <b-dropdown-item href="#">Settings</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
+</template>
